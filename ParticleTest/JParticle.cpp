fcf09409becb8412b461d4f4048f4e957ef8627c
Seth Lewis <lewisfamily@Seths-Mac-mini.T-mobile.com>
2016-08-26 11:25:24 -0700

    removed unused files

---
+++ ParticleTest/JParticle.cpp
@@ -0,0 +1,9 @@
+//
+//  JParticle.cpp
+//  ParticleTest
+//
+//  Created by Seth Lewis on 8/16/16.
+//  Copyright © 2016 Jackson Lewis. All rights reserved.
+//
+
+#include "JParticle.hpp"
---
+++ ParticleTest/JParticle.hpp
@@ -0,0 +1,14 @@
+//
+//  JParticle.hpp
+//  ParticleTest
+//
+//  Created by Seth Lewis on 8/16/16.
+//  Copyright © 2016 Jackson Lewis. All rights reserved.
+//
+
+#ifndef JParticle_hpp
+#define JParticle_hpp
+
+#include <stdio.h>
+
+#endif /* JParticle_hpp */
